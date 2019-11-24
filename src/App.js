ac29c55060012d2594eac2bdd6c5cd9a2653b42e
robbie <robbiekruszynski@gmail.com>
2019-11-23 22:57:02 -0800

    add form and buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "rimble-ui";
+import { ThemeProvider } from "rimble-ui";
+import { Button, Form } from "rimble-ui";
+import { Box } from "rimble-ui";
 
 function App() {
   return (
     <div className="App">
-      <Button size={"medium"}>Click me!</Button>
+      <ThemeProvider>
+        <Form>
+          <Form.Field label="Email" width={1}>
+            <Form.Input type="email" required width={1} />
+          </Form.Field>
+          <Form.Field label="Password" width={1}>
+            <Form.Input type="password" required width={1} />
+          </Form.Field>
+          <Box>
+            <Form.Check label="Remember me?" mb={3} />
+          </Box>
+          <Button type="submit" width={1}>
+            Sign Up
+          </Button>
+        </Form>
+      </ThemeProvider>
     </div>
   );
 }
